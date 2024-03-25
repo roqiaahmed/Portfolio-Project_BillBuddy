cfeac37ef4e157635e33725c1a2a40948ef11482
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-03-25 08:26:53 +0200

    create swagger doce

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-require("dotenv").config();
-require("express-async-errors");
+require('dotenv').config();
+require('express-async-errors');
 
-const { initialize } = require("./utils/notificationService");
-const express = require("express");
-const connectDb = require("./db/connect");
+const { initialize } = require('./utils/notificationService');
+const express = require('express');
+const connectDb = require('./db/connect');
 
 //router
-const router = require("./routes/index");
+const router = require('./routes/index');
 
-const admin = require("firebase-admin");
+//firebase
+const admin = require('firebase-admin');
+
+//Swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
 
 //error handling middleware
-const notFoundMiddleware = require("./middleware/not-found");
-const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 const app = express();
 
@@ -23,11 +28,13 @@ admin.initializeApp({
 });
 
 app.use(express.json());
-app.use("/api/v1", router);
+app.use('/api/v1', router);
 
-app.get("/", (req, res) => {
-  res.send("hi");
+app.get('/', (req, res) => {
+  res.send('hi');
 });
+
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //error handling middleware
 app.use(notFoundMiddleware);
