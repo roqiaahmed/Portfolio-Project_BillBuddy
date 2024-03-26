b6d75d64957cfb4d29561c6243a37af193de259d
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-03-26 17:22:39 +0200

    minor change (from "" to '')

--- models/user.js
+++ models/user.js
@@ -1,32 +1,32 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Name is required"],
-      minlength: [3, "Minimum length is 3 characters"],
+      required: [true, 'Name is required'],
+      minlength: [3, 'Minimum length is 3 characters'],
     },
     email: {
       type: String,
-      required: [true, "Email is required"],
+      unique: true,
+      required: [true, 'Email is required'],
       match: [
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Please provide a valid email",
+        'Please provide a valid email',
       ],
-      unique: true,
     },
     password: {
       type: String,
-      required: [true, "Password is required"],
-      minlength: [6, "Minimum length is 6 characters"],
+      required: [true, 'Password is required'],
+      minlength: [6, 'Minimum length is 6 characters'],
     },
   },
   { timestamps: true }
 );
 
-userSchema.pre("save", async function () {
+userSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
@@ -39,4 +39,4 @@ userSchema.methods.comparePassword = async function (canditatePassword) {
   return isPasswordValid;
 };
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
